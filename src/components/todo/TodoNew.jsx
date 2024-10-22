a827cb4764e325631d9f2ba1a85756aae5c96f5f
Dino? <daophuduytan2203@gmail.com>
2024-10-22 22:06:47 +0700

    su dung dom event

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -2,12 +2,22 @@
 
 const TodoNew = (props) => {
   console.log("check out ", props) ;
-  const {addNewTodo} =props ;  
-  addNewTodo("eric")
-    return(
+  const { addNewTodo } =props ;  
+  // addNewTodo("eric")
+  const handleclick = () => {
+    alert("click me")
+  }
+  const handleOnChange = (event) =>{
+    console.log(`>>> handleOnChange` , event.target.value)
+  }
+    return( 
         <div className='todo-new'>
-        <input type="text" />
-        <button>Add</button>
+        <input type="text" 
+        onChange={handleOnChange} 
+        />
+        <button style = {{cursor : "pointer"}} 
+        onClick={handleclick}
+        >Add</button>
       </div>
     )
 }
