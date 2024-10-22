7a0451085b386af50adb39feaac854228f80be76
Dino? <daophuduytan2203@gmail.com>
2024-10-22 21:29:02 +0700

    hoc props

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,17 @@ import TodoData from './components/todo/TodoData'
 import TodoNew from './components/todo/TodoNew'
 import ReactLogo from './assets/react.svg'
 const App = () => {
+
+  const tandao = "duy tan"
+  const age = 23
+  
   return (
     <div className="todo-container">
       <div className="todo-title ">Todo list</div>
       <TodoNew/>
-      <TodoData/>
+      <TodoData
+      name = {tandao}
+      age = {age} />
       <div className='todo-image'>
         <img src={ReactLogo} className='logo'/>
       </div>
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,6 +1,9 @@
-const TodoData = () => {
+const TodoData = (props) => {
+  //props la` 1 bien ojects 
+    const {name , age , data } = props ; 
     return(
         <div className="todo-data">
+        <div>My name is {name} {age} {data}</div>
         <div>Learning React</div>
         <div>Abc</div>
       </div>
