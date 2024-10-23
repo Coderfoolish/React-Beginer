836e180f7c99f48568f73be5b571c2efcea778aa
Dino? <daophuduytan2203@gmail.com>
2024-10-23 20:50:55 +0700

    tao router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,40 @@
-import './components/todo/todo.css'
-import TodoData from './components/todo/TodoData'
-import TodoNew from './components/todo/TodoNew'
-import ReactLogo from './assets/react.svg'
-import { useState } from 'react'
+import "./components/todo/todo.css";
+import TodoData from "./components/todo/TodoData";
+import TodoNew from "./components/todo/TodoNew";
+import ReactLogo from "./assets/react.svg";
+import { useState } from "react";
 const App = () => {
-
-  const tandao = "duy tan"
-  const age = 23
-  const [todoList , setTodoList] = useState([
-  {id : 1 , name: "abc"} , 
-  {id : 2 , name: "cdm"}
-  ])
+  // const tandao = "duy tan";
+  // const age = 23;
+  const [todoList, setTodoList] = useState([
+    { id: 1, name: "abc" },
+    { id: 2, name: "cdm" },
+  ]);
   const addNewTodo = (name) => {
     const newTodo = {
-      id : randomIntFromInterval(1 , 1000000),
-      name: name 
-    }
-    setTodoList([...todoList , newTodo])
-  }
-  const randomIntFromInterval = (min, max) => { // min and max included 
+      id: randomIntFromInterval(1, 1000000),
+      name: name,
+    };
+    setTodoList([...todoList, newTodo]);
+  };
+  const randomIntFromInterval = (min, max) => {
+    // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min);
-  }
+  };
 
   const deleteTodo = (id) => {
-      const newTodo = todoList.filter(item => item.id !== id) 
-      setTodoList(newTodo)
-  }
+    const newTodo = todoList.filter((item) => item.id !== id);
+    setTodoList(newTodo);
+  };
   return (
     <div className="todo-container">
       <div className="todo-title ">Todo list</div>
-      <TodoNew
-       addNewTodo = {addNewTodo} 
-       />
-      <TodoData
-      todoList={todoList}
-      deleteTodo = {deleteTodo} 
-       />
-      <div className='todo-image'>
-        <img src={ReactLogo} className='logo'/>
+      <TodoNew addNewTodo={addNewTodo} />
+      <TodoData todoList={todoList} deleteTodo={deleteTodo} />
+      <div className="todo-image">
+        <img src={ReactLogo} className="logo" />
       </div>
     </div>
-  )
-}
-export default App
+  );
+};
+export default App;
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,20 +1,22 @@
 const TodoData = (props) => {
-
-  const { todoList , deleteTodo } = props;
+  const { todoList, deleteTodo } = props;
   // console.log(">> check props", todoList);
   const handleclick = (id) => {
-    deleteTodo(id)
-  }
+    deleteTodo(id);
+  };
   return (
     <div className="todo-data">
       {todoList.map((item, index) => {
         console.log(">> check map : ", item, index);
         return (
-          <div className={`todo-item`} key= {item.id}>
+          <div className={`todo-item`} key={item.id}>
             <div>{item.name}</div>
-            <button 
-            onClick={() => handleclick(item.id)}
-            style={{ cursor: "pointer" }}>delete</button>
+            <button
+              onClick={() => handleclick(item.id)}
+              style={{ cursor: "pointer" }}
+            >
+              delete
+            </button>
           </div>
         );
       })}
